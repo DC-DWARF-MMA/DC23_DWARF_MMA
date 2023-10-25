712f45a84dd691a59ee888b27dbef67d1ef63604
Marek Brandt <marek7119@gmail.com>
2023-10-25 21:00:50 +0200

    new app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from './firebase';
+import Button from '@mui/material/Button';
 
-function App() {
+const App = () => {
+
+  const saveToDb = async ()  => {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Dada",
+        last: "Lacelove",
+        born: Math.floor(Math.random() * 123) + 1900
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <h1>Dwarf-mma</h1>
+        <section>
+          <Button variant="contained" onClick={saveToDb}>Click here to save to db</Button>
+        </section>
     </div>
   );
 }
