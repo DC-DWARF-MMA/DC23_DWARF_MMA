c99add85fca67e9cfe4c423454abb2387ebfb03e
Marek Brandt <marek7119@gmail.com>
2023-11-05 14:10:11 +0100

    simple invoice

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,33 @@ import { Navbar } from "../shared/Navbar";
 import { UserContracts } from "./UserContracts";
 import { UserEmailInputForm } from "../forms/UserEmailInputForm";
 import "../App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { JSON_DATA_HEADER } from "../models/constants";
 
 export const Home = () => {
   const mail = useUser();
+  const [processId, setProcessId] = useState();
+
+  const startProcess = async () => {
+    const data = await axios
+      .post(
+        "http://localhost:8080/engine-rest/process-definition/key/Process_190revr/start",
+        {},
+        JSON_DATA_HEADER
+      )
+      .catch(() => alert("Could not find deployed process definition"));
+    if (!!data) {
+      const result = await data.data.id;
+      setProcessId(result);
+      console.log(result);
+    }
+  };
+
+  useEffect(() => {
+    //this will make new process instance with every refresh...
+    startProcess();
+  }, []);
 
   return (
     <div className="App">
